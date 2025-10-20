0d137b3f23dfa51489c8b65db4e616b21768deac
MOHIT DHAMI <148179834+mohitdhami2611@users.noreply.github.com>
2025-10-20 19:52:03 +0530

    created navbar

---
+++ Navbar/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="" alt="logo" />
+      <div>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Products</li>
+            <li>User</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
